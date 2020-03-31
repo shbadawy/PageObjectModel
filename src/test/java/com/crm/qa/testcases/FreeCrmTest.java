d82a7bcbb159ba0dda008d435d4bb247f574a4d9
Shimaa Badawy <sweetgirlloly96@gmail.com>
2020-03-31 22:14:50 +0300

    Update FreeCrmTest.java

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -28,7 +28,7 @@ public class FreeCrmTest {
 		//System.setProperty("webdriver.chrome.driver", "C:\\Utility\\BrowserDrivers\\chromedriver.exe");
 		//System.setProperty("webdriver.chrome.driver", "/home/shimaa/Selenuim jars/chromedriver");
 		//*****************************************
-		System.setProperty("webdriver.gecko.driver", "geckodriver");
+		System.setProperty("webdriver.gecko.driver", "/var/lib/jenkins/workspace/maven-test-latest/geckodriver");
 		 driver = new FirefoxDriver();
 		//*****************************************
 	
