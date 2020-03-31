81442aa3df495049cd754e57a6579932af259d5a
Shimaa Badawy <sweetgirlloly96@gmail.com>
2020-03-31 15:58:07 +0300

    Update FreeCrmTest.java

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -20,7 +20,7 @@ public class FreeCrmTest {
 
 	@BeforeMethod
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/home/shimaa/Selenuim jars/chromedriver");
 		driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
 		driver.get("https://www.freecrm.com/index.html");
@@ -40,50 +40,6 @@ public class FreeCrmTest {
 			takeScreenshot("freecrmloginpage");
 			Assert.assertTrue(false);
 		}
-
-	}
-
-	public static void getRunTimeInfoMessage(String messageType, String message) throws InterruptedException {
-		// Check for jQuery on the page, add it if need be
-		js.executeScript("if (!window.jQuery) {"
-				+ "var jquery = document.createElement('script'); jquery.type = 'text/javascript';"
-				+ "jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js';"
-				+ "document.getElementsByTagName('head')[0].appendChild(jquery);" + "}");
-		Thread.sleep(5000);
-
-		// Use jQuery to add jquery-growl to the page
-		js.executeScript("$.getScript('https://the-internet.herokuapp.com/js/vendor/jquery.growl.js')");
-
-		// js.executeScript("$.getScript('/Users/NaveenKhunteta/Documents/workspace/Test/src/testcases/jquery.growl.js')");
-
-		// Use jQuery to add jquery-growl styles to the page
-		js.executeScript("$('head').append('<link rel=\"stylesheet\" "
-				+ "href=\"https://the-internet.herokuapp.com/css/jquery.growl.css\" " + "type=\"text/css\" />');");
-		Thread.sleep(5000);
-
-		// jquery-growl w/ no frills
-		js.executeScript("$.growl({ title: 'GET', message: '/' });");
-		
-		if(messageType.equals("error")){
-			js.executeScript("$.growl.error({ title: 'ERROR', message: '"+message+"' });");
-		}else if(messageType.equals("info")){
-			js.executeScript("$.growl.notice({ title: 'Notice', message: '"+message+"' });");
-		}else if(messageType.equals("warning")){
-			js.executeScript("$.growl.warning({ title: 'Warning!', message: '"+message+"' });");
-		}
-
-		// jquery-growl w/ colorized output
-//		js.executeScript("$.growl.error({ title: 'ERROR', message: 'Some exception is coming' });");
-//		js.executeScript("$.growl.notice({ title: 'Notice', message: 'your notice message goes here' });");
-//		js.executeScript("$.growl.warning({ title: 'Warning!', message: 'your warning message goes here' });");
-	}
-	
-	public static void takeScreenshot(String fileName) throws IOException{
-		// Take screenshot and store as a file format
-		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		// now copy the screenshot to desired location using copyFile //method
-		FileUtils.copyFile(src, 
-				new File("/Users/NaveenKhunteta/Documents/MyPOMFramework/PageObjectModel/screenshots/" + fileName +".png"));
 
 	}
 
